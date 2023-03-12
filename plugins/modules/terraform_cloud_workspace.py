001f355b4d724d932b83dce9da89e148467e6726
arpan <arpan.rec@gmail.com>
2023-03-13 01:32:54 +0530

    module_utils

--- plugins/modules/terraform_cloud_workspace.py
+++ plugins/modules/terraform_cloud_workspace.py
@@ -5,7 +5,7 @@
 from __future__ import absolute_import, division, print_function
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.hashicorp_tfe_core import crud
+from ansible_collections.arpanrec.utilities.plugins.module_utils.hashicorp_tfe_core import crud
 
 __metaclass__ = type
 
